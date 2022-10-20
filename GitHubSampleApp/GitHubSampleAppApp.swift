b6cb70cba0da2d72634da8140effddfe412b570e
YouheiShikano <65435549+YouheiShikano@users.noreply.github.com>
2022-10-21 01:28:52 +0900

    Initial Commit

---
+++ GitHubSampleApp/GitHubSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSampleAppApp.swift
+//  GitHubSampleApp
+//
+//  Created by 鹿野耀平 on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
